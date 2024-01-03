411108c6ad7ea951eaa2a4fc3550494feff7f52b
sumankathirvelbeyos <155507475+sumankathirvelbeyos@users.noreply.github.com>
2024-01-03 10:10:53 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -14,7 +14,11 @@ const port = 8010;
 
 app.use(express.json());
 app.use(cookiParser());
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization'
+}));
 app.use(router);
 
 
